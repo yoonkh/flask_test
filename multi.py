1f94a6788d1ae41eb1d20bb8a182825055715006
yoonkh <zizou0812@gmail.com>
2017-10-26 16:40:45 +0900

    multi.py URL test

--- multi.py
+++ multi.py
@@ -3,10 +3,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/multi')
-@app.route('/multi/<user>')
-def multi(user=None):  # None by default
-    return render_template('user.html', user=user)
+@app.route('/shopping')
+def shopping():
+    food = ["Cheese", "Tuna", "Beef"]
+    return render_template('shopping.html', food=food)
 
 
 if __name__ == '__main__':
