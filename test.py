36bce24a3ce62bb0668fa78b42b931e7e67d3ac9
yoonkh <zizou0812@gmail.com>
2017-10-26 12:20:56 +0900

    variable test + create def show_user_profile , create def show_post

--- test.py
+++ test.py
@@ -7,14 +7,23 @@ app = Flask(__name__)
 # def test():
 #     return 'tesing'
 
-@app.route('/')
-def index():
-    return 'Index Page'
+# @app.route('/')
+# def index():
+#     return 'Index Page'
+#
+#
+# @app.route('/test')
+# def test():
+#     return 'Testing'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
 
 
-@app.route('/test')
-def test():
-    return 'Testing'
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
 
 
 if __name__ == '__main__':
