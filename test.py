25fd2e5b28b3d43894d9c4e1fcd610a58ebc8735
yoonkh <zizou0812@gmail.com>
2017-10-26 12:28:05 +0900

    URL redirection test

--- test.py
+++ test.py
@@ -16,14 +16,23 @@ app = Flask(__name__)
 # def test():
 #     return 'Testing'
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return 'User %s' % username
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     return 'User %s' % username
+#
+#
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post %d' % post_id
+@app.route('/about')
+def about():
+    return 'The about page'
 
 
 if __name__ == '__main__':
