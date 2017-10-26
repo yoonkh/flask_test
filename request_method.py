51043d3444605eaab915cf0a4bcddd3e82de381e
yoonkh <zizou0812@gmail.com>
2017-10-26 16:30:34 +0900

    create request_method.py

---
+++ request_method.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # 아래의 코드는 요청이 GET 이거나, 인증정보가 잘못됐을때 실행된다.
+    return render_template('login.html', error=error)
