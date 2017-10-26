7bea9a7b2f76023b2fe5d0db8e4c8dc4a0a8549f
yoonkh <zizou0812@gmail.com>
2017-10-26 14:59:48 +0900

    create Debug.py file

--- create_URL.py
+++ create_URL.py
@@ -20,3 +20,4 @@ with app.test_request_context():
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
+
--- test.py
+++ test.py
@@ -25,15 +25,21 @@ app = Flask(__name__)
 # def show_post(post_id):
 #     return 'Post %d' % post_id
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
+# @app.route('/projects/')
+# def projects():
+#     return 'The project page'
+#
+#
+# @app.route('/about')
+# def about():
+#     return 'The about page'
 
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
 
 
 if __name__ == '__main__':
