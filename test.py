73a58df41004444e9fbbb35121b9922018070c78
yoonkh <zizou0812@gmail.com>
2017-10-26 12:12:46 +0900

    routing test

--- test.py
+++ test.py
@@ -3,9 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def test():
+#     return 'tesing'
+
 @app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/test')
 def test():
-    return 'tesing'
+    return 'Testing'
 
 
 if __name__ == '__main__':
