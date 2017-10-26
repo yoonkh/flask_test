15f1e26831b02fc8e9f3be19360db93f22410085
yoonkh <zizou0812@gmail.com>
2017-10-26 16:20:03 +0900

    create request_test.py file + test

---
+++ request_test.py
@@ -0,0 +1,18 @@
+from os import environ
+
+from flask import request, Flask
+
+app = Flask(__name__)
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block. such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+
+# with app.request_context(environ):
+#     assert request.method == 'POST'
+
+
+# if __name__ == '__main__':
+#     app.run()
