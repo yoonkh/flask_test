52d01af98ab84d2c40dbad48346a033ac949b73b
yoonkh <zizou0812@gmail.com>
2017-10-27 14:46:26 +0900

    add class NameForm + validators

--- test.py
+++ test.py
@@ -4,11 +4,21 @@ from flask_script import Manager
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
 
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 # from flask.ext.script import Manager
 # from flask import Flask
