229eda27c6bf53b582bda4b7a1b72d289970705c
yoonkh <zizou0812@gmail.com>
2017-10-27 10:19:50 +0900

    create request_view.py

---
+++ request_view.py
@@ -0,0 +1,14 @@
+from flask import request, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+
+if __name__ == '__main__':
+    app.run()
+
