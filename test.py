47d876a0749627529931e6e99d4186824dff3cef
yoonkh <zizou0812@gmail.com>
2017-10-30 12:45:39 +0900

    create db table + row

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
+app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
