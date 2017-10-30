c0a8f8f5b3c5121ccab7b07d838c57cbea1a6767
yoonkh <zizou0812@gmail.com>
2017-10-30 14:00:34 +0900

    test.py >> def index edit + index.html edit

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
+app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
@@ -118,14 +118,19 @@ class User(db.Model):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html',
-                           form=form, name=session.get('name'))
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.errorhandler(404)
