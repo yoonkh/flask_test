2736332f39c7e17e307b63dde10a9b15ef4087f7
yoonkh <zizou0812@gmail.com>
2017-11-01 15:42:14 +0900

    app/models.py permission constants

--- app/models.py
+++ app/models.py
@@ -5,6 +5,14 @@ from flask_login import UserMixin
 from . import db, login_manager
 
 
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
